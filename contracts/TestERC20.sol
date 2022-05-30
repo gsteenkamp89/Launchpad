7b8103e2d8fd4c9b2142d727c3b81ee630591199
Gerhard_Steenkamp <gerhard.m.steenkamp@gmail.com>
2022-05-30 14:24:45 +0200

    initial commit - deploy OTT

---
+++ contracts/TestERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract MockERC20 is ERC20, ERC20Permit {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 totalSupply_
+    ) ERC20(name_, symbol_) ERC20Permit(name_) {
+        _mint(msg.sender, totalSupply_);
+        emit Transfer(address(this), msg.sender, totalSupply_);
+    }
+}
